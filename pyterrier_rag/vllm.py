66e4e86f711ff32097b7bd76a28aab081bfd2d66
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-05-06 11:47:27 +0100

    boom

--- pyterrier_rag/vllm.py
+++ pyterrier_rag/vllm.py
@@ -1,9 +1,18 @@
 from .base import BaseModel
-from typing import Any
+from typing import List
 
 class VLLMModel(BaseModel):
-    def __init__(self, model_name_or_path: str, config: Any = None, generation_args: dict = ..., batch_size: int = 1) -> None:
-        super().__init__(model_name_or_path, config, generation_args, batch_size)
-        self.model = None
-        self.tokenizer = None
+    def __init__(self, model_name_or_path: str, model_args : dict = {}, generation_args: dict = ..., batch_size: int = 1) -> None:
+        super().__init__(model_name_or_path, None, generation_args, batch_size)
+        from vllm import LLM, SamplingParams, EngineArgs, LLMEngine
 
+        args = EngineArgs(**model_args)
+        engine = LLMEngine.from_engine_args(args)
+    
+        self.generation_args = SamplingParams(**generation_args)
+        self.model = LLM(engine, self.generation_args)
+
+    def generate(self, inps : List[str]) -> List[str]:
+        outputs = self.model.generate(inps, self.generation_args)
+
+        return [output[0].text for output in outputs]
